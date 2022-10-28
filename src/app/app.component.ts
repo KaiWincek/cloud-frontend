2a8607e6deddf35cbe5c88a2669064ff4ac9ff21
KaiWincek <kai.wincek@gmail.com>
2022-10-28 14:57:12 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {FormBuilder, FormControl} from "@angular/forms";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Cloud';
+  value = 'hey boi';
+  input = new FormControl();
+
+  cloudForm = this.formBuilder.group({
+    input: this.input
+  });
+
+  constructor(private http: HttpClient,
+              private formBuilder: FormBuilder) {}
+
+  save(){
+    this.http.post<string>(environment.backendURL + '/save', this.input.value, { headers: new HttpHeaders({ 'Content-Type': 'application/json;charset=utf-8' }) }).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
